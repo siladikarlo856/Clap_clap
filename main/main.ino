8aca0ff486536d12624de21bf39d71b8d0102ed1
siladikarlo856 <karlo.siladi@fer.hr>
2017-02-17 23:58:25 +0100

    Added clap-clap functionality. Need to add some comments.

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,8 @@
 /*Constants*/
 const byte ledPin = 13;
 const byte clapPin = 2;
-const int debounceTime = 100;
-
+const int debounceTime = 70;
+const int clapTime = 600;
 /*Functions*/
 
 
@@ -20,11 +20,26 @@ void loop() {
 
 void clap() {
    static unsigned long last_interrupt_time = 0;
+   static unsigned long first_interrupt_time = 0;
+   static unsigned long second_interrupt_time = 0;
+   
     unsigned long interrupt_time = millis();
-    // If interrupts come faster than 200ms, assume it's a bounce and ignore
-    if (interrupt_time - last_interrupt_time > debounceTime) 
-    {
-      toggleLight();
+   
+    // If interrupts come faster than debounceTime ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > debounceTime) {
+       if(first_interrupt_time  == 0 && second_interrupt_time == 0) {
+          first_interrupt_time = interrupt_time;
+       } else if (interrupt_time - first_interrupt_time > clapTime){
+          first_interrupt_time = interrupt_time;
+       } else if (second_interrupt_time == 0) {
+          second_interrupt_time = interrupt_time;
+       }
+       
+      if(second_interrupt_time - first_interrupt_time < clapTime) {
+        toggleLight();
+        first_interrupt_time = 0;
+        second_interrupt_time = 0;
+      }
     }
  last_interrupt_time = interrupt_time;
 }
